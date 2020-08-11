8631a053af782900879237864803a0f7ff10f443
El Diablo <estesquefawer@gmail.com>
2020-08-11 16:02:07 -0500

    deployed to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 8081,
   user: "root",
   password: "a123456b",
   database: "burgers_db"
-});
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
